7a07d4db9a6c4ec6e69e44ef0371a38ab34e3817
Hari haran S G <145080913+Hari220504@users.noreply.github.com>
2025-11-20 11:34:49 +0530

    Update trainee-routes.js

--- routes/trainee-routes.js
+++ routes/trainee-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const TraineeController = require('../controllers/trainees-controller');
 
 router.get('/readAllTrainees', TraineeController.readAllTrainees);
-router.get('/readTrainee', TraineeController.readATrainee);
+router.post('/readTrainee', TraineeController.readATrainee);
 router.post('/addTrainee', TraineeController.addATrainee);
 router.put('/updateTrainee', TraineeController.updateATrainee);
 router.delete('/deleteTrainee', TraineeController.deleteATrainee);
